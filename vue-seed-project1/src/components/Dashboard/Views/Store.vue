2da40df28adbdb9aa26a2c6baf508b89a0d9ed59
AmrNoaman <amr.abd-elhamid@student.guc.edu.eg>
2018-03-02 23:23:01 +0200

    front end done

--- vue-seed-project1/src/components/Dashboard/Views/Store.vue
+++ vue-seed-project1/src/components/Dashboard/Views/Store.vue
@@ -1,144 +1,72 @@
 <template>
-  <div>
-    <!-- <div class="row">
+    <div class="row">
       <div class="col-md-12">
         <div class="card">
+          <paper-table :title="table1.title" :sub-title="table1.subTitle" :data="table1.data" :columns="table1.columns">
 
+          </paper-table>
         </div>
       </div>
-    </div> -->
-    <div>
-      <input type="text" v-model= "name" > Name <br>
-      <input type="text" v-model= "price"> Price <br>
-      <input type="text" v-model= "deleteP"> Delete Product <br>
-      <input type="text" v-model= "updatePname"> Update Name <br>
-      <input type="text" v-model= "updatePprice"> Update Price <br>
-      <input type="text" v-model= "updatePid"> ID of product to be updated <br>
-      <button v-on:click="AddToApi"> add product</button>
-      <button v-on:click="DeleteP"> delete product</button>
-      <button v-on:click="UpdateP"> update product</button>
+
     </div>
-
-    <div>
-    <table id="firstTable" style="width:100%">
-    <thead>
-      <tr>
-        <th>CreatedAt</th>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Price</th>
-        <th>SellerName</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="row in rows.data">
-        <td>{{row.createdAt}}</td>
-        <td>{{row._id}}</td>
-        <td>{{row.name}}</td>
-        <td>{{row.price}}</td>
-        <td>{{row.sellerID}}</td>
-        <!-- <button v-on:click="DeleteP"> delete product</button> -->
-      </tr>
-    </tbody>
-    </table>
-  </div>
-</div>
-
 </template>
-
 <script>
-
-import axios from 'axios'
-import PaperTable from 'components/UIComponents/PaperTable.vue'
-const tableColumns = ['Id', 'Name', 'Price', 'CreatedAt', 'UpdatedAt', 'SellerName', '']
-const tableData = [{
-  id: 1,
-  name: 'Dakota Rice',
-  price: '$36.738',
-  createdat: '15/2/2018',
-  updatedat: '16/2/2018',
-  sellername: 'Mo'
-},
-{
-  id: 2,
-  name: 'Minerva Hooper',
-  price: '$36.738',
-  createdat: '15/2/2018',
-  updatedat: '16/2/2018',
-  sellername: 'Mo'
-}]
-export default {
-  components: {
-    PaperTable
+  import PaperTable from 'components/UIComponents/PaperTable.vue'
+  const tableColumns = ['Id', 'Name', 'Price', 'CreatedAt', 'UpdatedAt', 'SellerName']
+  const tableData = [{
+    id: 1,
+    name: 'Backpack',
+    price: '$30',
+    createdat: '15/2/2018',
+    updatedat: '16/2/2018',
+    sellername: 'Nour Mostafa'
   },
-  data () {
-    return {
-      table1: {
-        title: 'Stripped Table',
-        subTitle: 'Here is a subtitle for this table',
-        columns: [...tableColumns],
-        data: [...tableData]
-      },
-      table2: {
-        title: 'Table on Plain Background',
-        subTitle: 'Here is a subtitle for this table',
-        columns: [...tableColumns],
-        data: [...tableData]
-      },
-      rows: [],
-      name: '',
-      price: '',
-      deleteid: ''
-    }
+  {
+    id: 2,
+    name: 'Clock',
+    price: '$40',
+    createdat: '11/2/2018',
+    updatedat: '17/4/2018',
+    sellername: 'Amr Noaman'
   },
-  created () {
-    this.getProd()
+  {
+    id: 3,
+    name: 'Laptops',
+    price: '$55',
+    createdat: '18/9/2018',
+    updatedat: '16/2/2018',
+    sellername: 'Sameh Sonbol'
   },
-  methods: {
-    AddToApi () {
-      axios.post('http://localhost:3000/api/product/createProduct', {
-        name: this.name,
-        price: this.price
-      }).then((response) => {
-        console.log(response)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+  {
+    id: 4,
+    name: 'Bottles',
+    price: '$70',
+    createdat: '19/2/2018',
+    updatedat: '13/2/2018',
+    sellername: 'Omar'
+  }
+]
+  export default {
+    components: {
+      PaperTable
     },
-    getProd () {
-      axios.get('http://localhost:3000/api/product/getProducts')
-      .then((response) => {
-        this.rows = response.data
-        console.log(response)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-    },
-    DeleteP () {
-      axios.delete('http://localhost:3000/api/product/deleteProduct/' + this.deleteP)
-      .then((response) => {
-        console.log(response)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-    },
-    UpdateP () {
-      axios.patch('http://localhost:3000/api/product/updateProduct/' + this.updatePid, {
-        name: this.updatePname,
-        price: this.updatePprice
-      })
-      .then((response) => {
-        console.log(response)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    data () {
+      return {
+        table1: {
+          title: 'Component 5',
+          subTitle: ' Amazon ',
+          columns: [...tableColumns],
+          data: [...tableData]
+        },
+        table2: {
+          title: 'Table on Plain Background',
+          subTitle: 'Here is a subtitle for this table',
+          columns: [...tableColumns],
+          data: [...tableData]
+        }
+      }
     }
   }
-}
 
 </script>
 <style>
